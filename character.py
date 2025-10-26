41abeddce64649b64c3f7e591ab95b1e970cc8f6
KIM SEONG CHAN <powerbible88@tukorea.ac.kr>
2025-10-26 16:37:17 +0900

    drill 08번을 바탕으로 기본 뼈대인 main, character, state_machine 구성(state_machine는 수업때 배운 코드를 기반으로 가져옴)

---
+++ character.py
@@ -0,0 +1,18 @@
+from sprite_tuples import ACTION, sprite, sweat
+from state_machine import StateMachine
+
+# 애니 좌표/액션 인덱스:
+
+
+class Character:
+    def __init__(self, pid=1):
+        pass
+
+    def handle_event(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
---
+++ main.py
@@ -0,0 +1,59 @@
+from pico2d import *
+from character import Character  # import only
+
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            pass
+
+
+
+
+
+def reset_world():
+    pass
+
+
+    world = []
+
+
+
+
+
+
+def update_world():
+    pass
+
+
+
+def render_world():
+    pass
+
+
+
+running = True
+
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
