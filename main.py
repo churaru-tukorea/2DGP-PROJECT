5b83576efb79509b90de2c604539830de68c57d7
KIM SEONG CHAN <powerbible88@tukorea.ac.kr>
2025-11-01 21:00:46 +0900

    기존의 main을 main과 play_mode로 분리

--- main.py
+++ main.py
@@ -1,53 +1,5 @@
 from pico2d import *
-from character import Character
-import game_world
-
-
-
-
-
-def handle_events():
-    global running
-
-    event_list = get_events()
-    for event in event_list:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            character.handle_event(event)  # space 키가 들어오면 소년에게 전달.
-
-
-
-
-
-def reset_world():
-
-    global character
-
-
-
-
-    character = Character()
-    game_world.add_object(character, 1)
-
-
-def update_world():
-    game_world.update()
-
-
-
-def render_world():
-    clear_canvas()
-    game_world.render()
-    update_canvas()
-
-
-
-running = True
-
-
+import play_mode
 
 open_canvas()
 reset_world()
