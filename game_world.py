f5afef5ad43b45202b4662bdf1868fc6c8e1fbc0
KIM SEONG CHAN <powerbible88@tukorea.ac.kr>
2025-11-08 13:46:16 +0900

    충돌을 구현하기 위해 수업때 배운 코드를 가져옴

--- game_world.py
+++ game_world.py
@@ -25,15 +25,68 @@ def render(): # 게임 월드의 모든 객체를 그리는 함수
             o.draw()
 
 
-def remove_object(o): # 게임 월드의 객체를 제거하는 함수
-    #하다 보면 프로그램 오류로 인해서 게임 월드에 들어있지도 않은 객체를 지우려고 할떄가 있다. 그래서 체크를 해야 한다.
+def remove_collision_object(o):
+  for pairs in collision_pairs.values():
+   if o in pairs[0]:
+     pairs[0].remove(o)
+   if o in pairs[1]:
+     pairs[1].remove(o)
+
+def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
-            return #return 하는 이유는 찾아지지 않았을 경우 에러를 출력하기 위해
-    raise Exception('월드에 존재하지 않은 객체를 삭제하려고 합니다.')
-    #이렇게 하는게 안전하다.
+            remove_collision_object(o)
+            return
+
+    raise ValueError('Cannot delete non existing object')
+
 
 def clear():
+    global world
+
     for layer in world:
         layer.clear()
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    return True
+
+collision_pairs = {}
+#boy:ball 이라는게 그룹 이름이 되고, 그 안에 키와 value로 들어가나?
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:#처음 추가되는 그룹이면
+        print(f'Added new group {group}')
+        collision_pairs[group] = [[], []]#해당 그룹에 대해서 초기화
+    if a:
+            collision_pairs[group][0].append(a)
+    if b:
+            collision_pairs[group][1].append(b)
+
+
+def handle_collisions():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+              if collide(a, b):
+                a.handle_collision(group, b)
+                b.handle_collision(group, a)
+
+#내가 보낸 그룹 안에 있는 그걸 지워버리는
+def remove_collision_object_once(self, group):
+    if group in collision_pairs:
+        pairs = collision_pairs[group]
+        if self in pairs[0]:
+            pairs[0].remove(self)
+            return True
+        if self in pairs[1]:
+            pairs[1].remove(self)
+            return True
+    return None
