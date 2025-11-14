da0b2dfd76094558db21c171be6d7510489176c1
KIM SEONG CHAN <powerbible88@tukorea.ac.kr>
2025-11-14 16:10:03 +0900

    시작 전에 아이템 모드로 갈 수 있도록(원래는 타이틀에서 바로 고르는 단계로 가야하긴 하는데... 창 테스트 하려고 하는거니까 일단 이렇게

--- title_mode.py
+++ title_mode.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import game_framework
 import play_mode
+import item_mode
 
 image = None
 
@@ -33,8 +34,16 @@ def handle_events():
     for event in event_list:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+
+        # 스페이스: 현재 설정된 무기 모드 그대로 게임 시작
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
             game_framework.change_mode(play_mode)
+
+        # i: 무기 선택 모드로 들어가기
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_i:
+            import item_mode
+            game_framework.change_mode(item_mode)
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
 
