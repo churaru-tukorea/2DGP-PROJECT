dc62b571c62a9b9eba3f26c3c7cd34ebcfa26fcc
KIM SEONG CHAN <powerbible88@tukorea.ac.kr>
2025-11-14 12:39:56 +0900

    items.py를 만들고 2가지 클래스의 기본적인 뼈대 작성

---
+++ items.py
@@ -0,0 +1,43 @@
+from pico2d import load_image, get_time, draw_rectangle
+import game_world
+
+
+
+class SpeedClockItem:
+    def __init__(self, x, y):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
+
+    def apply_to(self, character):
+        pass
+
+class AttackClockItem:
+    def __init__(self, x, y):
+        pass
+
+    def update(self):
+
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
+
+    def apply_to(self, character):
+        pass
